1491bff1c88c2ad6dfd672a6d869621782c771ab
kdo_1999/mac <kdo_1999@naver.com>
2023-11-11 21:58:46 +0900

    상품 도메인 개발

---
+++ src/main/java/hello/itemservice/domain/item/ItemRepository.java
@@ -0,0 +1,43 @@
+package hello.itemservice.domain.item;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Repository
+public class ItemRepository {
+    private static final Map<Long, Item> store = new HashMap<>(); //동시성 문제로 인해 실제로는 ConcurrentHashMap 사용할 것
+    private static long sequence = 0L; //동시성 문제로 인해 실제로는 AtomicLong 사용할 것
+
+    private static AtomicLong atomicLong = new AtomicLong(0);
+
+    public Item save(Item item) {
+        item.setId(++sequence);
+        store.put(item.getId(), item);
+        return item;
+    }
+
+    public Item findById(Long id) {
+        return store.get(id);
+    }
+
+    public List<Item> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void update(Long itemId, Item updateParam) {
+        Item findItem = findById(itemId);
+        findItem.setItemName(updateParam.getItemName());
+        findItem.setPrice(updateParam.getPrice());
+        findItem.setQuantity(updateParam.getQuantity());
+    }
+
+    public void clearStore() {
+        store.clear();
+    }
+}
